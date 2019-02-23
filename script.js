c00021e4287c4c196b308f63ebce5b622cb6b3c4
TinX <TinX@TinX.local>
2019-02-23 16:32:52 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write a program that prins all the numbers from 1 to 100. For multiples of 3, instead of the number, print 'Fizz', for multiples of 5 print 'Buzz'. For numbers which are multiples of both 3 and 5, print 'FizzBuzz'. 
+
+function fizzBuzz() {}
+
+// Call Function
+
+const output = maxCharacter('javascript'); 
+
+console.log(output); 
+
